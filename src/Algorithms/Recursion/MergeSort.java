0e1b9854fac307bcad26c67920e4d1cc29ac3943
yasiruwickramasinghe <it19038292@my.sliit.lk>
2023-12-14 16:01:25 +0530

    merge sort and quick sort updated on recursion algo

---
+++ src/Algorithms/Recursion/MergeSort.java
@@ -0,0 +1,154 @@
+package Algorithms.Recursion;
+
+public class MergeSort {
+
+    /*
+        Description:
+        Merge Sort is a divide-and-conquer algorithm that divides the array into two halves,
+        recursively sorts them, and then merges the sorted halves.
+
+        Steps:
+        1. Divide: Split the array into two halves.
+        2. Recursively Sort: Apply Merge Sort to each half.
+        3. Merge: Merge the sorted halves into a single sorted array.
+
+        Pseudo Code:
+        function mergeSort(arr: list, low: int, high: int)
+            if low < high
+                mid = (low + high) / 2
+
+                // Recursively sort the left and right halves
+                mergeSort(arr, low, mid)
+                mergeSort(arr, mid + 1, high)
+
+                // Merge the sorted halves
+                merge(arr, low, mid, high)
+        end function
+
+        function merge(arr: list, low: int, mid: int, high: int)
+            // Find sizes of two subarrays to be merged
+            n1 = mid - low + 1
+            n2 = high - mid
+
+            // Create temporary arrays
+            leftArray = new int[n1]
+            rightArray = new int[n2]
+
+            // Copy data to temporary arrays
+            for i from 0 to n1 - 1
+                leftArray[i] = arr[low + i]
+            end for
+            for j from 0 to n2 - 1
+                rightArray[j] = arr[mid + 1 + j]
+            end for
+
+            // Merge the temporary arrays back into the main array
+            i = 0
+            j = 0
+            k = low
+            while i < n1 and j < n2
+                if leftArray[i] <= rightArray[j]
+                    arr[k] = leftArray[i]
+                    i = i + 1
+                else
+                    arr[k] = rightArray[j]
+                    j = j + 1
+                end if
+                k = k + 1
+            end while
+
+            // Copy the remaining elements of leftArray[], if any
+            while i < n1
+                arr[k] = leftArray[i]
+                i = i + 1
+                k = k + 1
+            end while
+
+            // Copy the remaining elements of rightArray[], if any
+            while j < n2
+                arr[k] = rightArray[j]
+                j = j + 1
+                k = k + 1
+            end while
+        end function
+
+        // Test
+        array = [7, 97 , 4, 11, 10, 51,  3, 75, 2, 7, 48]
+        print("Before Sorting Array: ", array)
+
+        mergeSort(array, 0, length(array) - 1)
+        print("After Sorting Array: ", array)
+    */
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            // Find the middle point
+            int mid = (low + high) / 2;
+
+            // Recursively sort the left and right halves
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+
+            // Merge the sorted halves
+            merge(arr, low, mid, high);
+        }
+    }
+
+    private static void merge(int[] arr, int low, int mid, int high) {
+        // Find sizes of two subarrays to be merged
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+
+        // Create temporary arrays
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        // Copy data to temporary arrays
+        System.arraycopy(arr, low, leftArray, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArray, 0, n2);
+
+        // Merge the temporary arrays back into the main array
+        int i = 0, j = 0, k = low;
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy the remaining elements of leftArray[], if any
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        // Copy the remaining elements of rightArray[], if any
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 97, 4, 11, 10, 51, 3, 75, 2, 7, 48};
+
+        System.out.println("Before Sorting Array:");
+        for (int element : arr) {
+            System.out.print(element + ",");
+        }
+        System.out.println();
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("After Sorting Array:");
+        for (int element : arr) {
+            System.out.print(element + ",");
+        }
+    }
+}
---
+++ src/Algorithms/Recursion/QuickSort.java
@@ -0,0 +1,165 @@
+package Algorithms.Recursion;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    // Description:
+        // Quick Sort is a divide-and-conquer algorithm that works by selecting a 'pivot' (pivot is central point of problem ) element
+        // and partitioning the other elements into two sub-arrays according to whether they are less than or greater than the pivot.
+        // The sub-arrays are then sorted recursively.
+
+    // Steps:
+        // Partitioning: Select a pivot element and partition the array into two sub-arrays. we use divide and conquer algorithm
+        // Recursive Sort: Recursively apply Quick Sort to the sub-arrays.
+        // Combine: No additional combine step is needed as the sorting is done in-place.
+
+
+        /*
+        Test:
+
+        Original array: [7, 4, 10, 3, 2, 11]
+
+        Pass 1:
+            Choose 11 as the pivot and partition the array.
+            -> [4, 7, 10, 3, 2, 11] (Partitioned: [4, 7, 10, 3, 2], Pivot: 11)
+
+            Recursively sort the left sub-array: [4, 7, 10, 3, 2]
+            Pass 1.1:
+                Choose 10 as the pivot and partition the sub-array.
+                -> [4, 7, 10, 3, 2] (Partitioned: [4, 7], Pivot: 10)
+                Recursively sort the left sub-array: [4, 7]
+                -> [4, 7]
+
+                Recursively sort the right sub-array: [3, 2]
+                Pass 1.2:
+                    Choose 2 as the pivot and partition the sub-array.
+                    -> [2, 3] (Partitioned: [2], Pivot: 3)
+                    Recursively sort the left sub-array: [2]
+                    -> [2]
+
+                    Recursively sort the right sub-array: [3]
+                    -> [3]
+
+                Combine sorted sub-arrays: [2, 3]
+                -> [2, 3, 4, 7, 10]
+
+            Combine sorted sub-arrays: [2, 3, 4, 7, 10, 11]
+            -> [2, 3, 4, 7, 10, 11]
+
+        Sorted array: [2, 3, 4, 7, 10, 11]
+      */
+
+
+    /*
+        Pseudo Code:
+
+        function quickSort(A: list, low: int, high: int)
+          if low < high
+              // Partition the array and get the index of the pivot element
+              pivotIndex = partition(A, low, high)
+
+              // Recursively sort the sub-arrays
+              quickSort(A, low, pivotIndex - 1)
+              quickSort(A, pivotIndex + 1, high)
+
+        end function
+
+        function partition(A: list, low: int, high: int) -> int
+          // Choose the rightmost element as the pivot
+          pivot = A[high]
+
+          // Initialize the index of the smaller element
+          i = low - 1
+
+          // Iterate through the array
+          for j from low to high - 1
+              // If the current element is smaller than or equal to the pivot
+              if A[j] <= pivot
+                //increment i value because in start i value is -1
+                  i = i + 1
+                  // Swap A[i] and A[j]
+                  swap(A[i + 1], A[j])
+
+              end if
+          end for
+
+          // Swap A[i + 1] and the pivot
+          swap(A[i + 1], A[high])
+
+          // Return the index of the pivot
+          return i + 1
+        end function
+
+        // Test
+        array = [7, 4, 10, 3, 2, 11]
+        print("Before Sorting Array: ", array)
+
+        quickSort(array, 0, length(array) - 1)
+        print("After Sorting Array: ", array)
+    */
+
+    public static int[] quickSort(int[] arr, int low, int high){
+        if(low < high){
+            // Partition the array and get the pivot index
+            int pivotIndex = partition(arr,low,high);
+
+            // Recursively sort the sub arrays on both sides of the pivot
+            quickSort(arr, low, pivotIndex-1 );
+            quickSort(arr,pivotIndex+1 , high);
+        }
+
+        return arr;
+
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        // Choose the rightmost element as the pivot
+        int pivot = arr[high];
+
+        // Initialize the index of the smaller element
+        int i = low - 1;
+
+        // Iterate through the array
+        for (int j = low; j < high; j++) {
+
+            // If the current element is smaller than or equal to the pivot
+            if (arr[j] < pivot) {
+                //increment i value because in start i value is -1
+                i++;
+
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Swap arr[i+1] and the pivot
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        System.out.println("within partitioning:" + Arrays.toString(arr));
+
+        // Return the index of the pivot
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arrs = {7, 97 , 4, 11, 10, 51,  3, 75, 2, 7, 48};
+
+        System.out.println("Before Sorting Array: ");
+        for(int arr: arrs){
+            System.out.print(arr+",");
+        }
+        System.out.println();
+
+        int[] sortedArrs = quickSort(arrs, 0, arrs.length-1);
+
+        System.out.println("After Sorting Array:");
+        for(int arr: sortedArrs){
+            System.out.print(arr+",");
+        }
+    }
+
+}
