ad9a532e81d35e51d71c99cdb36680b74a28e277
yasiruwickramasinghe <it19038292@my.sliit.lk>
2024-01-04 12:58:32 +0530

    Quick sort emplemented in in-place

--- src/Algorithms/_2_Sorting_Algorithms/_6_Quick_Sort/_1_Base_Algorithm/QuickSort.java
+++ src/Algorithms/_2_Sorting_Algorithms/_6_Quick_Sort/_1_Base_Algorithm/QuickSort.java
@@ -53,127 +53,75 @@ public class QuickSort {
 
         Pseudo Code:
 
-        function quickSort(A: list, low: int, high: int)
-             n = length(A)
+     procedure quickSort(arr, low, high)
+     if low >= high
+     return
+     start = low
+     end = high
+     mid = start + (end - start) / 2
+     pivot = arr[mid]  // Choose any random number, last element, or middle element as the pivot
 
-             // check whether array is empty
-             if n equals 0
-                return new empty array
-             end if
+     while start <= end
+     while arr[start] < pivot
+     start++
+     while arr[end] > pivot
+     end--
 
-              if low < high
-                  // Partition the array and get the index of the pivot element
-                  pivotIndex = partition(A, low, high)
+     if start <= end
+     swap arr[start] and arr[end]
+     start++
+     end--
 
-                  // Recursively sort the sub-arrays
-                  quickSort(A, low, pivotIndex - 1)
-                  quickSort(A, pivotIndex + 1, high)
+     // Now pivot is at correct index, sort two halves
+     quickSort(arr, low, end)
+     quickSort(arr, start, high)
 
-        end function
+     procedure main
+     arr = {5, 4, 3, 2, 1}
+     quickSort(arr, 0, length(arr) - 1)
+     print(arr)
 
-        function partition(A: list, low: int, high: int) -> int
-          // Choose the rightmost element as the pivot
-          pivot = A[high]
-
-          // Initialize the index of the smaller element
-          i = low - 1
-
-          // Iterate through the array
-          for j from low to high - 1
-              // If the current element is smaller than or equal to the pivot
-              if A[j] <= pivot
-                //increment i value because in start i value is -1
-                  i = i + 1
-                  // Swap A[i] and A[j]
-                  swap(A[i + 1], A[j])
-
-              end if
-          end for
-
-          // Swap A[i + 1] and the pivot
-          swap(A[i + 1], A[high])
-
-          // Return the index of the pivot
-          return i + 1
-        end function
-
-        // Test
-        array = [7, 4, 10, 3, 2, 11]
-        print("Before Sorting Array: ", array)
-
-        quickSort(array, 0, length(array) - 1)
-        print("After Sorting Array: ", array)
     **/
 
-    public static int[] quickSort(int[] arr, int low, int high) {
-        int size = arr.length;
-
-        //check whether array is empty
-        if (size == 0) {
-            return new int[0];
-        }
-
-        if (low < high) {
-            // Partition the array and get the pivot index
-            int pivotIndex = partition(arr, low, high);
-
-            // Recursively sort the sub arrays on both sides of the pivot
-            quickSort(arr, low, pivotIndex - 1);
-            quickSort(arr, pivotIndex + 1, high);
-        }
-
-        return arr;
-
+    public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        quickSort(arr,0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
     }
 
-    private static int partition(int[] arr, int low, int high) {
-        // Choose the rightmost element as the pivot
-        int pivot = arr[high];
+    static void quickSort(int[] arr, int low, int high){
+        if(low >= high){
+            return;
+        }
 
-        // Initialize the index of the smaller element
-        int i = low - 1;
+        int start = low;
+        int end = high;
+        int mid = start + (end - start) / 2;
+        int pivot = arr[mid]; // he we can use any random number or last element or mid any element
 
-        // Iterate through the array
-        for (int j = low; j < high; j++) {
+        while(start <= end){
 
-            // If the current element is smaller than or equal to the pivot
-            if (arr[j] < pivot) {
-                //increment i value because in start i value is -1
-                i++;
+            //also reason why if it already sorted it will not swap
+            while (arr[start] < pivot){
+                start++;
+            }
+            while (arr[end] > pivot){
+                end--;
+            }
 
-                // Swap arr[i] and arr[j]
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+            //swap
+            if(start <= end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
             }
         }
 
-        // Swap arr[i+1] and the pivot
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-
-        System.out.println("within partitioning:" + Arrays.toString(arr));
-
-        // Return the index of the pivot
-        return i + 1;
-    }
-
-    public static void main(String[] args) {
-        int[] arrs = {7, 97, 4, 11, 10, 51, 3, 75, 2, 7, 48};
-
-        System.out.println("Before Sorting Array: ");
-        for (int arr : arrs) {
-            System.out.print(arr + ",");
-        }
-        System.out.println();
-
-        int[] sortedArrs = quickSort(arrs, 0, arrs.length - 1);
-
-        System.out.println("After Sorting Array:");
-        for (int arr : sortedArrs) {
-            System.out.print(arr + ",");
-        }
+        //now pivot is at correct index please sort two halves now
+        quickSort(arr, low, end);
+        quickSort(arr,start, high);
     }
 
 }
--- src/Algorithms/_4_Recursion/_6_Quick_Sort/Main.java
+++ src/Algorithms/_4_Recursion/_6_Quick_Sort/Main.java
@@ -1,7 +1,90 @@
 package Algorithms._4_Recursion._6_Quick_Sort;
 
+import java.util.Arrays;
+
+/**
+ Description:
+ Quick Sort is a divide-and-conquer algorithm that works by selecting a 'pivot' (pivot is central point of problem ) element
+ and partitioning the other elements into two sub-arrays according to whether they are less than or greater than the pivot.
+ The sub-arrays are then sorted recursively.
+
+ Steps:
+ Partitioning: Select a pivot element and partition the array into two sub-arrays. we use divide and conquer algorithm
+ Recursive Sort: Recursively apply Quick Sort to the sub-arrays.
+ Combine: No additional combine step is needed as the sorting is done in-place.
+
+ Pseudocode:
+
+     procedure quickSort(arr, low, high)
+     if low >= high
+     return
+     start = low
+     end = high
+     mid = start + (end - start) / 2
+     pivot = arr[mid]  // Choose any random number, last element, or middle element as the pivot
+
+     while start <= end
+     while arr[start] < pivot
+     start++
+     while arr[end] > pivot
+     end--
+
+     if start <= end
+     swap arr[start] and arr[end]
+     start++
+     end--
+
+     // Now pivot is at correct index, sort two halves
+     quickSort(arr, low, end)
+     quickSort(arr, start, high)
+
+     procedure main
+     arr = {5, 4, 3, 2, 1}
+     quickSort(arr, 0, length(arr) - 1)
+     print(arr)
+
+
+ **/
+
 public class Main {
     public static void main(String[] args) {
+        int[] arr = {5,4,3,2,1};
+        quickSort(arr,0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
 
+    static void quickSort(int[] arr, int low, int high){
+        if(low >= high){
+            return;
+        }
+
+        int start = low;
+        int end = high;
+        int mid = start + (end - start) / 2;
+        int pivot = arr[mid]; // he we can use any random number or last element or mid any element
+
+        while(start <= end){
+
+            //also reason why if it already sorted it will not swap
+            while (arr[start] < pivot){
+                start++;
+            }
+            while (arr[end] > pivot){
+                end--;
+            }
+
+            //swap
+            if(start <= end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        //now pivot is at correct index please sort two halves now
+        quickSort(arr, low, end);
+        quickSort(arr,start, high);
     }
 }
